9b3636ce11257eac394cba225633dbd5e1a71127
Héctor López <hector.lopez@dsa-dev2-10>
2019-08-07 17:52:39 -0500

    recreated all project

--- newlibrary/src/main/java/com/hector/newlibrary/Interfaces/IToolbar.kt
+++
@@ -1,8 +0,0 @@
-package com.hector.newlibrary.Interfaces
-
-import androidx.appcompat.widget.Toolbar
-
-interface IToolbar {
-    fun toolbarToLoad(toolbar: Toolbar?)
-    fun enableHomeDisplay(value:Boolean)
-}
